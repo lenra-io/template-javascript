1af176de5a0c77c3701a9932c6a4ee57827bc7bc
Thomas DA ROCHA <thomas.darocha@lenra.io>
2023-04-06 11:14:48 +0200

    feat: Upgrade components lib

--- views/counter.js
+++ views/counter.js
@@ -3,14 +3,14 @@
 const { Flex, Text, Button } = require("@lenra/components")
 
 module.exports = ([counter], { text }) => {
-  return Flex(
+  return Flex([
     Text(`${text}: ${counter.count}`),
     Button("+")
       .onPressed("increment", {
         "id": counter._id,
         "datastore": counter.datastore
       })
-  )
+  ])
     .spacing(16)
     .mainAxisAlignment("spaceEvenly")
     .crossAxisAlignment("center")
