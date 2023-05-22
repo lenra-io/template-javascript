d8991440e2a720995a0f9b8f65865dcffcfca67a
Emric Pichonnier <emric.pichonnier@gmail.com>
2023-05-22 14:37:40 +0200

    fix: api

--- src/listeners/increment.js
+++ src/listeners/increment.js
@@ -10,11 +10,11 @@ import { Counter } from "../classes/Counter.js";
  * @returns 
  */
 export default async function (props, _event, api) {
-    let token = await axios.post(`${this.api.url}/app/transaction`, { headers: { Authorization: `Bearer ${api.token}` } })
+    let token = await axios.post(`${api.url}/app/transaction`, { headers: { Authorization: `Bearer ${api.token}` } })
         .then(resp => resp.data);
     let counter = await api.data.getDoc(Counter, props.id);
     counter.count += 1;
-    await axios.put(`${this.api.url}/app/colls/counter/docs/${doc._id}`, doc, { headers: { Authorization: `Bearer ${token}` } });
-    await axios.put(`${this.api.url}/app/transacation/abort`, { headers: { Authorization: `Bearer ${token}` } });
+    await axios.put(`${api.url}/app/colls/counter/docs/${doc._id}`, doc, { headers: { Authorization: `Bearer ${token}` } });
+    await axios.put(`${api.url}/app/transacation/abort`, { headers: { Authorization: `Bearer ${token}` } });
     return {};
 }
