991fb0ac1ab96f038b24337ef078e0277f5d84ed
Emric Pichonnier <emric.pichonnier@gmail.com>
2023-05-22 09:37:23 +0200

    feat: transaction

--- src/listeners/increment.js
+++ src/listeners/increment.js
@@ -9,9 +9,12 @@ import { Counter } from "../classes/Counter.js";
  * @param {import("@lenra/app-server").Api} api
  * @returns 
  */
-export default async function(props, _event, api) {
+export default async function (props, _event, api) {
+    let token = await axios.post(`${this.api.url}/app/transaction`, { headers: { Authorization: `Bearer ${api.token}` } })
+        .then(resp => resp.data);
     let counter = await api.data.getDoc(Counter, props.id);
     counter.count += 1;
-    await api.data.updateDoc(counter);
+    await axios.put(`${this.api.url}/app/colls/counter/docs/${doc._id}`, doc, { headers: { Authorization: `Bearer ${token}` } });
+    await axios.put(`${this.api.url}/app/transacation/abort`, { headers: { Authorization: `Bearer ${token}` } });
     return {};
 }
